78e83dd7b214273e724b89c8dc8b5f46c1cd99fa
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2023-01-06 12:17:23 -0700

    Added listen

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Linda Rasmussen");
+    res.send("Linda Rasmussen");
+});
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000");
 });
