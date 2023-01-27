bb9ef4549cfecb80b02415c352dcb4c2d3998729
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2023-01-27 12:49:10 -0700

    Added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const app = express();
 
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", require("./routes/index"));
 app.use("/contacts", require("./routes/contacts"));
 app.listen(8080, function () {
